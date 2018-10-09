4b2db559d05865a9e5f67816972b9a9a5e68d4c5
skobraf <skobraf@gmail.com>
2018-10-09 14:39:35 +0100

    set up the user sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicatoinController
+	
+	def new
+	end
+
+	def create
+		user = User.find_by(email: params[:session][:email].downcase)
+		if user 
+			session[:user_id] = user.id
+			flash[:success] = 'You are logged in!'
+			redirect_to root_path
+		else
+			flash.now[:danger] = "Email is wrong!"
+			redirect_to 'new'
+		end
+	end
+
+	def destroy
+		session[:user_id] = nil
+		flash[:success] = 'You are now logged out'
+	end
+end
