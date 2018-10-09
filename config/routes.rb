89beddaafa6b1585a3d5b2a76a772e1bc3a19492
skobraf <skobraf@gmail.com>
2018-10-09 14:23:20 +0100

    added routs for login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
  root 'users#home'
 
  resources :users, only: [:new, :create]
+
+ get 'login', to: "sessions#new"
+ post 'login', to: "sessions#create"
+ delete 'logout', to: "sessions#destroy"
+ get 'signup', to: "users#new"
 end
