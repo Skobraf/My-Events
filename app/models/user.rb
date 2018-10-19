2bf79f0f9730326d968500612e0a214668365131
skobraf <skobraf@gmail.com>
2018-10-19 23:47:57 +0100

    added associations to event and user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+	has_many :hosted_events, class_name: 'Event', foreign_key: 'creator_id'
 	before_save { self.email = email.downcase }
 	validates :name, presence: true,
 				 length: {minimum: 3, maximum: 25},
