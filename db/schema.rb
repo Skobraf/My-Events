6519b26d9722ced33e832c651c3c429ca44fcfa5
skobraf <skobraf@gmail.com>
2018-10-20 13:29:12 +0100

    added creator_id to events table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_09_163510) do
+ActiveRecord::Schema.define(version: 2018_10_20_122645) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2018_10_09_163510) do
     t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "creator_id"
   end
 
   create_table "users", force: :cascade do |t|
